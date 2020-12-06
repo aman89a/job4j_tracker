e7f16d4a101dfc746130e061dd2264441d6ce9b7
aman <abdulaev25@gmail.com>
2020-12-06 12:06:10 +0600

    7. toString [#378992]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -5,8 +5,10 @@ import java.time.format.DateTimeFormatter;
 public class StartUI {
 
     public static void main(String[] args) {
+        Item item = new Item();
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
-        String currentDateTimeFormat = new Item().getCreated().format(formatter);
+        String currentDateTimeFormat = item.getCreated().format(formatter);
         System.out.println(currentDateTimeFormat);
+        System.out.println(item.toString());
     }
 }
