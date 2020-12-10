19cea47fe2ba782b2980bb9eb0c39982c25e9007
aman <abdulaev25@gmail.com>
2020-12-10 15:11:45 +0600

    2. Scanner и чтение числа из консоли. [#378925]

--- src/main/java/ru/job4j/io/Dendy.java
+++ src/main/java/ru/job4j/io/Dendy.java
@@ -6,24 +6,26 @@ public class Dendy {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int rsl = 11;
+        String value = "Player 1";
         while (rsl >= 1) {
+            System.out.println("player1 ");
             int player1 = Integer.valueOf(input.nextLine());
             if (player1 > 0 && player1 < 4) {
-                System.out.println("player1 " + player1);
                 rsl = rsl - player1;
-                System.out.println(rsl);
+                value = "Player-1";
             } else {
                 System.out.println("не правилный ввод данных");
             }
+            System.out.println("player2 ");
             int player2 = Integer.valueOf(input.nextLine());
             if (player2 > 0 && player2 < 4) {
-                System.out.println("player2 " + player2);
                 rsl = rsl - player2;
-                System.out.println(rsl);
+                value = "Player-2";
+
             } else {
                 System.out.println("не правилный ввод данных");
             }
-            System.out.println(rsl);
         }
+        System.out.println("Player " + value + " is win.");
     }
 }
