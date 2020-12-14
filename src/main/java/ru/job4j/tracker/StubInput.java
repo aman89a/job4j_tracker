ff5519eb2a5d78d1cbe20b9f265411b6cb657b41
aman <abdulaev25@gmail.com>
2020-12-14 13:54:30 +0600

    5.1. Тестирование. Подготовка данных. [#378933]

--- src/main/java/ru/job4j/tracker/StubInput.java
+++ src/main/java/ru/job4j/tracker/StubInput.java
@@ -9,10 +9,12 @@ public class StubInput implements Input {
     }
     @Override
     public String askStr(String question) {
+
         return answers[position++];
     }
     @Override
     public int askInt(String question) {
+
         return Integer.parseInt(askStr(question));
     }
 }
