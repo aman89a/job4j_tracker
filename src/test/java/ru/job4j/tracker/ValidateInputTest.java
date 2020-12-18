22d62f7ac217a8399de6c2001be6f54b50effd27
aman <abdulaev25@gmail.com>
2020-12-18 20:14:10 +0600

    2.1. Тест на ValidateInput [#378950]

---
+++ src/test/java/ru/job4j/tracker/ValidateInputTest.java
@@ -0,0 +1,60 @@
+package ru.job4j.tracker;
+
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+public class ValidateInputTest {
+
+    @Test
+    public void whenInvalidInput() {
+        Output out = new StubOutput();
+        Input in = new StubInput(
+                new String[] {"one", "1"}
+        );
+        ValidateInput input = new ValidateInput(out, in);
+        int selected = input.askInt("Enter menu:");
+        assertThat(selected, is(1));
+    }
+    @Test
+    public void whenValidInput() {
+        Output out = new StubOutput();
+        Input in = new StubInput(
+                new String[] {"1"}
+        );
+        ValidateInput input = new ValidateInput(out, in);
+        int selected = input.askInt("Enter menu:");
+        assertThat(selected, is(1));
+    }
+
+    @Test
+    public void whenValidInputs() {
+        Output out = new StubOutput();
+        Input in = new StubInput(
+                new String[] {"1","2","3"}
+        );
+        ValidateInput input = new ValidateInput(out, in);
+        int selected = input.askInt("Enter menu:");
+        int selected1 = input.askInt("Enter menu:");
+        int selected2 = input.askInt("Enter menu:");
+        assertThat(selected, is(1));
+        assertThat(selected1, is(2));
+        assertThat(selected2, is(3));
+    }
+
+    @Test
+    public void whenInValidInputs() {
+        Output out = new StubOutput();
+        Input in = new StubInput(
+                new String[] {"10","20","30"}
+        );
+        ValidateInput input = new ValidateInput(out, in);
+        int selected = input.askInt("Enter menu:");
+        int selected1 = input.askInt("Enter menu:");
+        int selected2 = input.askInt("Enter menu:");
+        assertThat(selected, is(10));
+        assertThat(selected1, is(20));
+        assertThat(selected2, is(30));
+    }
+}
