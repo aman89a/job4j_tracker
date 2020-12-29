a5c0d97c1d844f6b4d4d3c3c9da22d8f527c6b9a
aman <abdulaev25@gmail.com>
2020-12-29 11:11:48 +0600

    2. Очередь с приоритетом на LinkedList [#378904]

--- src/main/java/ru/job4j/search/PriorityQueue.java
+++ src/main/java/ru/job4j/search/PriorityQueue.java
@@ -12,9 +12,8 @@ public class PriorityQueue {
 
             if (element.getPriority() < task.getPriority()) {
                 index++;
-            } else {
-                break;
             }
+            break;
         }
         this.tasks.add(index, task);
     }
