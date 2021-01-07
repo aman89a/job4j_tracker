b9aad55bbf091f5acc4ded4c34aec40bbb1d56db
aman <abdulaev25@gmail.com>
2021-01-07 12:07:42 +0600

    2. Комбинированный компаратор [#378900]

--- src/test/java/ru/job4j/collection/JobTest.java
+++ src/test/java/ru/job4j/collection/JobTest.java
@@ -12,19 +12,18 @@ public class JobTest {
     public void whenCompatorByNameAndPrority() {
         Comparator<Job> cmpNamePriority = new JobDescByName().thenComparing(new JobDescByPriority());
         int rsl = cmpNamePriority.compare(
-                new Job("Impl task", 0),
-                new Job("Fix bug", 1)
+                new Job("Impl task", 2),
+                new Job("Impl task", 1)
         );
-        System.out.println(rsl);
         assertThat(rsl, lessThan(0));
     }
 
     @Test
-    public void whenCompatorByNameAscAndProrityAsc() {
-        Comparator<Job> cmpNamePriorityAsc = new JobAscByName().thenComparing(new JobAscByPriority());
-        int rsl = cmpNamePriorityAsc.compare(
-                new Job("Impl task", 0),
-                new Job("Fix bug", 1)
+    public void whenCompatorByPriorityAndName() {
+        Comparator<Job> cmpPriorityNameAsc = new JobAscByPriority().thenComparing(new JobAscByName());
+        int rsl = cmpPriorityNameAsc.compare(
+                new Job("d", 1),
+                new Job("b", 1)
         );
         assertThat(rsl, greaterThan(0));
     }
@@ -32,12 +31,23 @@ public class JobTest {
     @Test
     public void whenComparatorByPriorityNameDesc() {
         List<Job> jobs = Arrays.asList(
-                new Job("Fix bugs", 4),
+                new Job("Fix bugs", 2),
                 new Job("Impl task", 2),
-                new Job("Reboot server", 1));
+                new Job("Reboot server", 2));
         Collections.sort(jobs, new JobDescByPriority().thenComparing(new JobDescByName()));
-        assertThat(jobs.toString(), is("[Job{Fix bugs, 4}, " +
-                "Job{Impl task, 2}, Job{Reboot server, 1}]"));
+        assertThat(jobs.toString(), is("[Job{Reboot server, 2}," +
+                " Job{Impl task, 2}, Job{Fix bugs, 2}]"));
+    }
+
+    @Test
+    public void whenComparatorByNameAndPriorityAsc() {
+        List<Job> jobs = Arrays.asList(
+                new Job("Fix bugs", 2),
+                new Job("Fix bugs", 4),
+                new Job("Fix bugs", 6));
+        Collections.sort(jobs, new JobAscByName().thenComparing(new JobAscByPriority()));
+        assertThat(jobs.toString(), is("[Job{Fix bugs, 2}," +
+                " Job{Fix bugs, 4}, Job{Fix bugs, 6}]"));
     }
 
 
